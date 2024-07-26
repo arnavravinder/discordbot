f7784f18e6e005faed15cc5e5a274f3159403f6a
Arnav <100010741+arnavravinder@users.noreply.github.com>
2024-07-26 18:07:22 +0530

    Update rpgbot.js

--- rpgbot.js
+++ rpgbot.js
@@ -5,7 +5,7 @@ const PREFIX = '!';
 client.commands = new Collection();
 
 client.once('ready', () => {
-    console.log('Bot is online!');
+    console.log('Bot is online! 💪');
 });
 
 const users = {};
@@ -25,19 +25,20 @@ client.on('messageCreate', async message => {
                 health: 100,
                 mana: 50,
                 inventory: [],
+                gold: 50,
             };
-            message.channel.send(`Welcome to the RPG, ${message.author.username}! Your adventure begins now.`);
+            message.channel.send(`Welcome to the RPG, ${message.author.username}! Your adventure begins now. 🏰`);
         } else {
-            message.channel.send(`You have already started your adventure, ${message.author.username}!`);
+            message.channel.send(`You have already started your adventure, ${message.author.username}! 🛡️`);
         }
     }
 
     if (command === 'profile') {
         const user = users[message.author.id];
         if (user) {
-            message.channel.send(`**${user.username}'s Profile**\nLevel: ${user.level}\nExperience: ${user.experience}\nHealth: ${user.health}\nMana: ${user.mana}\nInventory: ${user.inventory.join(', ') || 'Empty'}`);
+            message.channel.send(`**${user.username}'s Profile**\nLevel: ${user.level} 🏅\nExperience: ${user.experience} ⭐\nHealth: ${user.health} ❤️\nMana: ${user.mana} 🔮\nGold: ${user.gold} 💰\nInventory: ${user.inventory.join(', ') || 'Empty'}`);
         } else {
-            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin.`);
+            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin. 🗡️`);
         }
     }
 
@@ -48,10 +49,11 @@ client.on('messageCreate', async message => {
             if (outcome === 'win') {
                 user.experience += 10;
                 user.health -= 10;
-                message.channel.send(`${user.username} fought bravely and won! You gained 10 experience but lost 10 health.`);
+                user.gold += 5;
+                message.channel.send(`${user.username} fought bravely and won! 🏆 You gained 10 experience, 5 gold, but lost 10 health.`);
             } else {
                 user.health -= 20;
-                message.channel.send(`${user.username} fought bravely but lost. You lost 20 health.`);
+                message.channel.send(`${user.username} fought bravely but lost. 😢 You lost 20 health.`);
             }
 
             if (user.experience >= user.level * 20) {
@@ -59,15 +61,133 @@ client.on('messageCreate', async message => {
                 user.experience = 0;
                 user.health = 100;
                 user.mana = 50;
-                message.channel.send(`Congratulations, ${user.username}! You leveled up to level ${user.level}. Your health and mana have been restored.`);
+                message.channel.send(`Congratulations, ${user.username}! 🎉 You leveled up to level ${user.level}. Your health and mana have been restored.`);
             }
 
             if (user.health <= 0) {
-                message.channel.send(`${user.username} has fallen in battle. Rest in peace, brave warrior.`);
+                message.channel.send(`${user.username} has fallen in battle. 🪦 Rest in peace, brave warrior.`);
                 delete users[message.author.id];
             }
         } else {
-            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin.`);
+            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin. 🗡️`);
+        }
+    }
+
+    if (command === 'shop') {
+        message.channel.send(`🛒 **Welcome to the shop!**\n1. Health Potion (10 gold) - Type !buy health\n2. Mana Potion (10 gold) - Type !buy mana\n3. Sword (50 gold) - Type !buy sword`);
+    }
+
+    if (command === 'buy') {
+        const user = users[message.author.id];
+        if (user) {
+            const item = args[0];
+            if (item === 'health') {
+                if (user.gold >= 10) {
+                    user.gold -= 10;
+                    user.inventory.push('Health Potion');
+                    message.channel.send(`${user.username} bought a Health Potion! 💊`);
+                } else {
+                    message.channel.send(`You don't have enough gold, ${user.username}. 💸`);
+                }
+            } else if (item === 'mana') {
+                if (user.gold >= 10) {
+                    user.gold -= 10;
+                    user.inventory.push('Mana Potion');
+                    message.channel.send(`${user.username} bought a Mana Potion! 💊`);
+                } else {
+                    message.channel.send(`You don't have enough gold, ${user.username}. 💸`);
+                }
+            } else if (item === 'sword') {
+                if (user.gold >= 50) {
+                    user.gold -= 50;
+                    user.inventory.push('Sword');
+                    message.channel.send(`${user.username} bought a Sword! ⚔️`);
+                } else {
+                    message.channel.send(`You don't have enough gold, ${user.username}. 💸`);
+                }
+            } else {
+                message.channel.send(`Invalid item, ${user.username}. 🛑`);
+            }
+        } else {
+            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin. 🗡️`);
+        }
+    }
+
+    if (command === 'use') {
+        const user = users[message.author.id];
+        if (user) {
+            const item = args[0];
+            const itemIndex = user.inventory.indexOf(item.charAt(0).toUpperCase() + item.slice(1));
+            if (itemIndex !== -1) {
+                if (item === 'health') {
+                    user.health += 20;
+                    if (user.health > 100) user.health = 100;
+                    user.inventory.splice(itemIndex, 1);
+                    message.channel.send(`${user.username} used a Health Potion! 💊 Health is now ${user.health}.`);
+                } else if (item === 'mana') {
+                    user.mana += 20;
+                    if (user.mana > 50) user.mana = 50;
+                    user.inventory.splice(itemIndex, 1);
+                    message.channel.send(`${user.username} used a Mana Potion! 💊 Mana is now ${user.mana}.`);
+                } else {
+                    message.channel.send(`Invalid item usage, ${user.username}. 🛑`);
+                }
+            } else {
+                message.channel.send(`You don't have that item, ${user.username}. ❌`);
+            }
+        } else {
+            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin. 🗡️`);
+        }
+    }
+
+    if (command === 'leaderboard') {
+        const sortedUsers = Object.values(users).sort((a, b) => b.level - a.level || b.experience - a.experience);
+        let leaderboard = '**🏆 Leaderboard 🏆**\n';
+        sortedUsers.slice(0, 10).forEach((user, index) => {
+            leaderboard += `${index + 1}. ${user.username} - Level ${user.level} (${user.experience} XP) - ${user.gold} gold\n`;
+        });
+        message.channel.send(leaderboard);
+    }
+
+    if (command === 'quest') {
+        const user = users[message.author.id];
+        if (user) {
+            const questOutcome = Math.random();
+            if (questOutcome < 0.3) {
+                const goldFound = Math.floor(Math.random() * 20) + 10;
+                user.gold += goldFound;
+                message.channel.send(`${user.username} went on a quest and found ${goldFound} gold! 💰`);
+            } else if (questOutcome < 0.6) {
+                const experienceGained = Math.floor(Math.random() * 20) + 10;
+                user.experience += experienceGained;
+                message.channel.send(`${user.username} went on a quest and gained ${experienceGained} experience! ⭐`);
+            } else {
+                const healthLost = Math.floor(Math.random() * 20) + 10;
+                user.health -= healthLost;
+                if (user.health <= 0) {
+                    message.channel.send(`${user.username} went on a quest and lost ${healthLost} health, leading to their demise. 🪦`);
+                    delete users[message.author.id];
+                } else {
+                    message.channel.send(`${user.username} went on a quest and lost ${healthLost} health. ❤️`);
+                }
+            }
+        } else {
+            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin. 🗡️`);
+        }
+    }
+
+    if (command === 'heal') {
+        const user = users[message.author.id];
+        if (user) {
+            if (user.gold >= 20) {
+                user.gold -= 20;
+                user.health = 100;
+                message.channel.send(`${user.username} has been fully healed for 20 gold. ❤️`);
+            } else {
+                message.channel.send(`You don't have enough gold to heal, ${user.username}. 💸`);
+            }
+        } else {
+            message.channel.send(`You need to start your adventure first, ${message.author.username}! Type !start to begin. 🗡️`);
         }
     }
 });
